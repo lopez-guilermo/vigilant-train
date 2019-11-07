4b606cb172e2d428e372e0bdcd93c74ccf834621
guillermo <gl874391@gmail.com>
2019-11-07 01:33:56 +0000

    initial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
 	def index
 		@places = Place.all
@@ -7,9 +8,9 @@ class PlacesController < ApplicationController
 	def new
 		@place = Place.new
 	end
-  
+
  def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
