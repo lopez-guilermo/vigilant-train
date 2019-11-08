bf9242a11973f78103dbce366ecc42f255b5f385
guillermo <gl874391@gmail.com>
2019-11-08 20:21:28 +0000

    initial commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ApplicationRecord
 	belongs_to :user
-	validates :name, presence: true
+	geocoded_by :address
+  after_validation :geocode
+
+	validates :name, presence: true, lenght: {minimun: 3}
+	validates :address, presence: true
+	validates :descripition, presence: true
 end
