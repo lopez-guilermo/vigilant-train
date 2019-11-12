52a02b6a78792b0b28f20e483850ee982217050e
guillermo <gl874391@gmail.com>
2019-11-12 17:58:44 +0000

    change lenght

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ApplicationRecord
 	geocoded_by :address
   after_validation :geocode
 
-	validates :name, presence: true 
+	validates :name, presence: true :name, length: {minimum 3}
 	validates :address, presence: true
 	validates :description, presence: true
 end
