db8fc4f2f442f882c4540e18f2bad3d0511deb8c
guillermo <gl874391@gmail.com>
2019-11-21 18:50:52 +0000

    photos

--- db/migrate/20191118220300_create_photos.rb
+++ db/migrate/20191118220300_create_photos.rb
@@ -1,9 +1,6 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-    	
-    	t.text :caption
-    	t.integer :place_id
 
       t.timestamps
     end
