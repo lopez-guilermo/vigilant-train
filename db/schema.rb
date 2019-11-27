e4651ebfacd899307700f9c69b6c475548295c1f
guillermo <gl874391@gmail.com>
2019-11-27 01:05:40 +0000

    places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_234534) do
+ActiveRecord::Schema.define(version: 2019_11_27_005955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,12 @@ ActiveRecord::Schema.define(version: 2019_11_21_234534) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "user_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -40,6 +43,8 @@ ActiveRecord::Schema.define(version: 2019_11_21_234534) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
